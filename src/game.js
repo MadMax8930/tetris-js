39010baa11f4df39b883de8a0a49ee9c2383fae8
Max Surnin <madmax8930@gmail.com>
2021-11-20 19:04:09 +0100

    all done

--- src/game.js
+++ src/game.js
@@ -1,36 +1,25 @@
 export default class Game {
 
-    score = 0;
-    lines = 0;
-    level = 0;
-    playField = this.createPlayField();
-    activePiece = this.createPiece();
-    nextPiece = this.createPiece();
+    static points = {
+        '1': 40,
+        '2': 100,
+        '3': 300,
+        '4': 1200
+    };
 
+    // score = 0;
+    // lines = 0;
+    // topOut = false;
+    // playField = this.createPlayField();
+    // activePiece = this.createPiece();
+    // nextPiece = this.createPiece();
+    
+    constructor() {
+        this.reset();
+    }
 
-    getState() {
-        const playField = this.createPlayField();
-        const { y: pieceY, x: pieceX, blocks } = this.activePiece;
-
-        for (let y = 0; y < this.playField.length; y++) {
-            playField[y] = [];
-
-            for (let x = 0; x < this.playField[y].length; x++) {
-                playField[y][x] = this.playField[y][x];                
-            }
-        }
-
-        for (let y = 0; y < blocks.length; y++) {
-            for (let x = 0; x < blocks[y].length; x++) {
-                if (blocks[y][x]) {
-                    playField[pieceY + y][pieceX + x] = blocks[y][x];
-                } 
-            }
-        }
-
-        return {
-            playField
-        };
+    get level() {
+        return Math.floor(this.lines * 0.3);
     }
 
     createPlayField() {
@@ -114,6 +103,33 @@ export default class Game {
         return piece;
     }
 
+    updatePieces() {
+        this.activePiece = this.nextPiece;
+        this.nextPiece = this.createPiece();
+    }
+
+    // OutOfBounds
+
+    hasCollision() {
+        const { y: pieceY, x: pieceX, blocks } = this.activePiece;
+    
+        for (let y = 0; y < blocks.length; y++) {
+            for (let x = 0; x < blocks[y].length; x++) {
+                if (
+                    blocks[y][x] && 
+                    ((this.playField[pieceY + y] === undefined || this.playField[pieceY + y][pieceX + x] === undefined) ||
+                    this.playField[pieceY + y][pieceX + x])
+                ) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    // Piece Movement
+
     movePieceLeft() {
         this.activePiece.x -= 1;
 
@@ -131,12 +147,20 @@ export default class Game {
     }
 
     movePieceDown() {
+        if (this.topOut) return;
+
         this.activePiece.y += 1;
 
         if (this.hasCollision()) {
             this.activePiece.y -= 1;
             this.lockPiece();
+            const clearedLines = this.clearLines();
+            this.updateScore(clearedLines);
             this.updatePieces();
+        }
+
+        if (this.hasCollision()) {
+            this.topOut = true;
         }
     }
 
@@ -162,24 +186,6 @@ export default class Game {
         }
     }
 
-    hasCollision() {
-        const { y: pieceY, x: pieceX, blocks } = this.activePiece;
-    
-        for (let y = 0; y < blocks.length; y++) {
-            for (let x = 0; x < blocks[y].length; x++) {
-                if (
-                    blocks[y][x] && 
-                    ((this.playField[pieceY + y] === undefined || this.playField[pieceY + y][pieceX + x] === undefined) ||
-                    this.playField[pieceY + y][pieceX + x])
-                ) {
-                    return true;
-                }
-            }
-        }
-        
-        return false;
-    }
-
     lockPiece() {
         // const blocks = this.activePiece.blocks;
         // const pieceY = this.activePiece.y;
@@ -195,8 +201,82 @@ export default class Game {
         }
     }
 
-    updatePieces() {
-        this.activePiece = this.nextPiece;
+    getState() {
+        const playField = this.createPlayField();
+        const { y: pieceY, x: pieceX, blocks } = this.activePiece;
+
+        for (let y = 0; y < this.playField.length; y++) {
+            playField[y] = [];
+
+            for (let x = 0; x < this.playField[y].length; x++) {
+                playField[y][x] = this.playField[y][x];                
+            }
+        }
+
+        for (let y = 0; y < blocks.length; y++) {
+            for (let x = 0; x < blocks[y].length; x++) {
+                if (blocks[y][x]) {
+                    playField[pieceY + y][pieceX + x] = blocks[y][x];
+                } 
+            }
+        }
+
+        return {
+            score: this.score,
+            level: this.level,
+            lines: this.lines,
+            nextPiece: this.nextPiece,
+            playField,
+            isGameOver: this.topOut
+        };
+    }
+
+    reset() {
+        this.score = 0;
+        this.lines = 0;
+        this.topOut = false;
+        this.playField = this.createPlayField();
+        this.activePiece = this.createPiece();
         this.nextPiece = this.createPiece();
+    }
+
+    clearLines() {
+        const rows = 20;
+        const columns = 10;
+        let lines = [];
+
+        for (let y = rows - 1; y >= 0; y--) {
+            let numberOfBlocks = 0;
+
+            for (let x = 0; x < columns; x++) {
+                if (this.playField[y][x] !== 0) {
+                    numberOfBlocks += 1;
+                }
+            }
+
+            if (numberOfBlocks === 0) {
+                break;
+            } else if (numberOfBlocks < columns) {
+                continue;
+            } else if (numberOfBlocks === columns) {
+                lines.unshift(y); //index of lines
+            }
+        }
+
+        for (let index of lines) {
+            this.playField.splice(index, 1);
+            this.playField.unshift(new Array(columns).fill(0));
+        }
+
+        return lines.length;
+    }
+
+    updateScore(clearedLines) {
+        console.log(this.level);
+        if (clearedLines > 0) {
+            this.score += Game.points[clearedLines] * (this.level + 1);
+            this.lines += clearedLines;
+            console.log(this.score, this.lines, this.level);
+        }
     }
 }
--- src/view.js
+++ src/view.js
@@ -20,34 +20,69 @@ export default class View {
         this.canvas.height = this.height;
         this.context = this.canvas.getContext('2d');
 
-        this.blockWidth = this.width / columns;
-        this.blockHeight = this.height / rows;
+        this.playFieldBorderWidth = 4;
+        this.playFieldX = this.playFieldBorderWidth;
+        this.playFieldY = this.playFieldBorderWidth;
+        this.playFieldWidth = this.width * 2 / 3;
+        this.playFieldHeight = this.height;
+        this.playFieldInnerWidth = this.playFieldWidth - this.playFieldBorderWidth * 2;
+        this.playFieldInnerHeight = this.playFieldHeight - this.playFieldBorderWidth * 2;
+
+        this.blockWidth = this.playFieldInnerWidth / columns;
+        this.blockHeight = this.playFieldInnerHeight / rows;
+
+        this.panelX = this.playFieldWidth + 10;
+        this.panelY = 0;
+        this.panelWidth = this.width / 3;
+        this.panelHeight = this.height;
 
         this.element.appendChild(this.canvas);
     }
 
-    render({ playField }) {
+    // Screen Behaviour
+
+    renderMainScreen(state) {
         this.clearScreen();
-        this.renderPlayField(playField);
+        this.renderPlayField(state);
+        this.renderPanel(state);
+    }
+
+    renderStartScreen() {
+        this.context.fillStyle = 'white';
+        this.context.font = '18px "Press Start 2P"';
+        this.context.textAlign = 'center';
+        this.context.textBaseline = 'middle';
+        this.context.fillText('Press ENTER to Start', this.width / 2, this.height / 2);
+    }
+
+    renderPauseScreen() {
+        this.context.fillStyle = 'rgba(0,0,0,0.75)';
+        this.context.fillRect(0, 0, this.width, this.height);
+
+        this.context.fillStyle = 'white';
+        this.context.font = '18px "Press Start 2P"';
+        this.context.textAlign = 'center';
+        this.context.textBaseline = 'middle';
+        this.context.fillText('Press ENTER to Resume', this.width / 2, this.height / 2);
+    }
+
+    renderEndScreen({ score }) {
+        this.clearScreen();
+
+        this.context.fillStyle = 'white';
+        this.context.font = '18px "Press Start 2P"';
+        this.context.textAlign = 'center';
+        this.context.textBaseline = 'middle';
+        this.context.fillText('GAME OVER', this.width / 2, this.height / 2 - 48);
+        this.context.fillText(`Score: ${score}`, this.width / 2, this.height / 2);
+        this.context.fillText('Press ENTER to Restart', this.width / 2, this.height / 2 + 48);
     }
 
     clearScreen() {
         this.context.clearRect(0, 0, this.width, this.height);
     }
 
-    renderPlayField(playField) {
-        for (let y = 0; y < playField.length; y++) {
-            const line = playField[y];
-            
-            for (let x = 0; x < line.length; x++) {
-                const block = line[x];
-                
-                if (block) {
-                    this.renderBlock(x * this.blockWidth, y * this.blockHeight, this.blockWidth, this.blockHeight, View.colors[block]);
-                }
-            }
-        }
-    }
+    // Blocks, PlayField and Panel
 
     renderBlock(x, y, width, height, color) {
         this.context.fillStyle = color;
@@ -56,5 +91,59 @@ export default class View {
 
         this.context.fillRect(x, y, width, height);
         this.context.strokeRect(x, y, width, height);
+    }
+
+    renderPlayField({ playField }) {
+        for (let y = 0; y < playField.length; y++) {
+            const line = playField[y];
+            
+            for (let x = 0; x < line.length; x++) {
+                const block = line[x];
+                
+                if (block) {
+                    this.renderBlock(
+                        this.playFieldX + (x * this.blockWidth), 
+                        this.playFieldY + (y * this.blockHeight), 
+                        this.blockWidth, 
+                        this.blockHeight, 
+                        View.colors[block]
+                    );
+                }
+            }
+        }
+
+        this.context.strokeStyle = 'white';
+        this.context.lineWidth = this.playFieldBorderWidth;
+        this.context.strokeRect(0, 0, this.playFieldWidth, this.playFieldHeight);
+    }
+
+    renderPanel({ level, score, lines, nextPiece }) {
+        this.context.textAlign = 'start';
+        this.context.textBaseline = 'top';
+        this.context.fillStyle = 'white';
+        this.context.font = '14px "Press Start 2P"';
+
+        this.context.fillText(`Score: ${score}`, this.panelX, this.panelY + 0);
+        this.context.fillText(`Lines: ${lines}`, this.panelX, this.panelY + 24);
+        this.context.fillText(`Level: ${level}`, this.panelX, this.panelY + 48);
+        this.context.fillText('Next:', this.panelX, this.panelY + 96);
+
+        for (let y = 0; y < nextPiece.blocks.length; y++) {
+            for (let x = 0; x < nextPiece.blocks[y].length; x++) {
+                const block = nextPiece.blocks[y][x];
+
+                if (block) {
+                    this.renderBlock(
+                        this.panelX + (x * this.blockWidth * 0.5),
+                        this.panelY + 100 + (y * this.blockHeight * 0.5),
+                        this.blockWidth * 0.5,
+                        this.blockHeight * 0.5,
+                        View.colors[block]
+                    );
+                }
+                
+            }
+            
+        }
     }
 }
